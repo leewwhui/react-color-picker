f51f3952b6d6725404375f66f2774e2bd5c8ceaf
Wenhui Li <leewwhui@gmail.com>
2022-09-18 14:12:30 +0800

    feat: ✨ publish config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,23 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   root: path.join(__dirname, "examples"),
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.tsx"),
+      name: "src",
+      fileName: (format) => `build.${format}.ts`,
+    },
+    outDir: path.resolve(__dirname, "dist"),
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ["react"],
+    },
+    cssCodeSplit: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
